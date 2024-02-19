8773bfedf8c57553c21e1e5f5374effb04ba83cf
kadri <kadri.ozcan@outlook.com>
2024-02-20 00:30:41 +0300

    requests to the server

--- frontend/todo-app/src/components/Welcome.jsx
+++ frontend/todo-app/src/components/Welcome.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
 export default function Welcome() {
   const params = useParams();
+
+  const [message, setMessage] = useState(null);
+
+  function getHelloWorld() {
+    console.log(axios.get("http://localhost:8080/hello"));
+  }
 
   return (
     <div className="welcome">
@@ -11,6 +18,12 @@ export default function Welcome() {
       <div>
         Your ToDos: <Link to="/todos">Click Here</Link>
       </div>
+      <div>
+        <button className="btn btn-success" onClick={getHelloWorld}>
+          Call Hello World
+        </button>
+      </div>
+      <div className="text-info">{message}</div>
     </div>
   );
 }
